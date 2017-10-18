65c047c153f0b4e7db8e6382cda1109fc93eb401
fredakilla <fredakilla@gmail.com>
2017-10-18 06:50:43 +0200

    remove unecessary code

--- spark/include/Rendering/Urho3D/SPK_Urho3D_PointRenderer.h
+++ spark/include/Rendering/Urho3D/SPK_Urho3D_PointRenderer.h
@@ -19,23 +19,11 @@ public:
 
     static Ref<IUrho3DPointRenderer> create(Urho3D::Context* context, float pointSize = 16.0f);
 
-    Urho3D::Material* getMaterial()
-    {
-        return _material;
-    }
-    void setMaterial(Urho3D::Material* material)
-    {
-        _material =  material;
-    }
-
-
-
 private:
 
     static const size_t NB_INDICES_PER_PARTICLE = 1;
     static const size_t NB_VERTICES_PER_PARTICLE = 1;
     PODVector<VertexElement> _elements;
-    Urho3D::SharedPtr<Material> _material;
 
     IUrho3DPointRenderer(Urho3D::Context* context = nullptr, float pointSize = 16.0f);
     IUrho3DPointRenderer(const IUrho3DPointRenderer& renderer);
@@ -43,7 +31,6 @@ private:
     virtual RenderBuffer* attachRenderBuffer(const Group& group) const override;
     virtual void render(const Group& group,const DataSet* dataSet,RenderBuffer* renderBuffer) const override;
     virtual void computeAABB(Vector3D& AABBMin,Vector3D& AABBMax,const Group& group,const DataSet* dataSet) const override;
-
 };
 
 
